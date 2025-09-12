abdc639a0cbb874af01007ef9252dfe632b262ea
Derren Christopher <18224040@std.stei.itb.ac.id>
2025-09-12 15:00:49 +0700

    Commit LATIHAN PAK RIZA

---
+++ LATIHAN PAK RIZA/Point.java
@@ -0,0 +1,16 @@
+//Derren Christopher Boenardi - 18224040
+//Latihan Kelas dan Objek (Point)
+public class Point {
+    public int x = 0;
+    public int y = 0;
+
+    public Point (int a, int b) {
+        x = a;
+        y = b;
+    }
+
+    public void printPoint() {
+        System.out.println("Point (" + x + " , " + y + ")");
+    }
+}
+
